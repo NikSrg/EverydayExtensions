6bee2762a60b2355c29b4edfab74de1ab3c3f968
Nick Sorge <nick.sorge@gmx.de>
2020-05-03 12:59:48 +0200

    Added Keychain class

---
+++ EverydayExtensions/Classes/Keychain.swift
@@ -0,0 +1,45 @@
+//
+//  Keychain.swift
+//  EverydayExtensions
+//
+//  Created by Nick Sorge on 03.05.20.
+//  Copyright © 2020 Nick Sorge. All rights reserved.
+//
+
+import Foundation
+
+public class Keychain {
+    // MARK: - Public Methods
+
+    @discardableResult public func save(data: Data, for key: String) -> Bool {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrAccount as String: key,
+                                    kSecValueData as String: data,
+                                    kSecAttrSynchronizable as String: kCFBooleanTrue!]
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            return false
+        }
+        return true
+    }
+
+    public func getData(for key: String) -> Data? {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrAccount as String: key,
+                                    kSecReturnData as String: kCFBooleanTrue!,
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecAttrSynchronizable as String: kCFBooleanTrue!]
+
+        var item: CFTypeRef?
+        SecItemCopyMatching(query as CFDictionary, &item)
+        return item as? Data
+    }
+
+    @discardableResult public func deleteData(for key: String) -> Bool {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrAccount as String: key,
+                                    kSecAttrSynchronizable as String: kCFBooleanTrue!]
+        let status = SecItemDelete(query as CFDictionary)
+        return status == errSecSuccess || status == errSecItemNotFound
+    }
+}
