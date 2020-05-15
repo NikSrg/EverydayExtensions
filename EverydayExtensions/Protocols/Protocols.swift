945ffa8fd999d06831abcdb848844ee35131790b
Nick Sorge <nick.sorge@gmx.de>
2020-05-15 10:27:17 +0200

    Updated CustomDebugDescription protocol

--- EverydayExtensions/Protocols/Protocols.swift
+++ EverydayExtensions/Protocols/Protocols.swift
@@ -11,7 +11,6 @@ import Foundation
 // MARK: - CustomDebugDescription
 
 public protocol CustomDebugDescription {
-    var customDebugDescription: String { get }
 }
 
 public extension CustomDebugDescription {
