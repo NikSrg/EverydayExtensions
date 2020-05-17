dc965f2b1797101c5a3d43b602c8e24b999f6c84
Nick Sorge <nick.sorge@gmx.de>
2020-05-17 16:28:31 +0200

    Adjusted UIColor+Extension for handling 6 digits long hex

--- EverydayExtensions/Extensions/UIColor+Extension.swift
+++ EverydayExtensions/Extensions/UIColor+Extension.swift
@@ -27,7 +27,21 @@ public extension UIColor {
         let hexString = hex.hasPrefix("#") ? hex : String(format: "#%@", hex)
         let start = hexString.index(hex.startIndex, offsetBy: 1)
         let hexColor = String(hexString[start...])
-        if hexColor.count == 8 {
+        if hexColor.count == 6 {
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
+
+            if scanner.scanHexInt64(&hexNumber) {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(1)
+
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            }
+        }
+        else if hexColor.count == 8 {
             let scanner = Scanner(string: hexColor)
             var hexNumber: UInt64 = 0
 
@@ -36,11 +50,12 @@ public extension UIColor {
                 g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
                 b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
                 a = CGFloat(hexNumber & 0x000000ff) / 255
-                
+
                 self.init(red: r, green: g, blue: b, alpha: a)
                 return
             }
         }
+
         self.init(r: 0, g: 0, b: 0)
     }
 }
